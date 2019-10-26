ec3852356ed3ca22bcd9218e906f446bc1f53894
swjtuyhz <1143875182@qq.com>
2019-10-26 12:43:25 +0800

    add spring cloud config server(SVN)
    add spring cloud config server(SVN)

---
+++ spring-cloud-config-server-svn/src/test/java/com/zyh/serversvn/SpringCloudConfigServerSvnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zyh.serversvn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConfigServerSvnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
